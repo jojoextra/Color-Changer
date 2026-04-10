f3f2e4240c6957a1f1545f7f5919b468b555cab9
PiKaJoe <50702649+jojoextra@users.noreply.github.com>
2026-04-10 10:57:00 +0700

    change background color

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ function changeColor() {
 }
 var colors = ["red", "blue", "green", "yellow"];
 var random = Math.floor(Math.random() * colors.length);
+document.body.style.backgroundColor = colors[random];
